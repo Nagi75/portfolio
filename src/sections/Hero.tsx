7925eb06d8d134b7ffa7d516f6ea3ac484455e0c
Nagi <nagisesihiro97@gmail.com>
2025-06-14 21:55:30 +0530

    Update introduction in Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Nagi, a developer based in Croatia with a passion for
+              Hi, I’m Nagi, a developer based in India with a passion for
               code.
             </p>
 
